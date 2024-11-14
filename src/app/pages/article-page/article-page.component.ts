25975b16d85a9a40e47ba1e272f4232e692aa834
yoyodes1000 <yoyodes1000@gmail.com>
2024-11-14 10:21:58 +0100

    quest OK

--- src/app/pages/article-page/article-page.component.ts
+++ src/app/pages/article-page/article-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Article } from '../../models/article.model';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-article-page',
@@ -11,13 +15,17 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 export class ArticlePageComponent {
   private route: ActivatedRoute = inject(ActivatedRoute);
   articleId!: number;
+  article$!: Observable<Article>;
+
+  http = inject(HttpClient);
+  apiService = inject(ApiService);
 
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.articleId = Number(params.get('id'));
     });
+    this.article$ = this.apiService.getArticleById(this.articleId);
   }
 
-  
 }
 
